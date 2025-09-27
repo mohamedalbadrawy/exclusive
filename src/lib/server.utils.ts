ad91ddc594bfdd171a3f66e066a09054310e692b
mohamedalbadrawy <mohamedalbadrawy4@gmail.com>
2025-09-27 15:48:52 +0300

    changes in token

--- src/lib/server.utils.ts
+++ src/lib/server.utils.ts
@@ -3,7 +3,10 @@ import { cookies } from "next/headers";
 
 
 export async function getDecoddedToken() {
-    const encodedToken = (await cookies()).get('next-auth.session-token')?.value;
+    const cookieStore = await cookies()
+    const encodedToken =
+        cookieStore.get('next-auth.session-token')?.value ||
+        cookieStore.get('__Secure-next-auth.session-token')?.value;
 
     const decodedToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET! });
     console.log(decodedToken!.token);
@@ -13,10 +16,10 @@ export async function getDecoddedToken() {
 
 export async function getUserToken() {
 
-    return ( await getDecoddedToken())?.token
+    return (await getDecoddedToken())?.token
 }
 
 export async function getUserId() {
 
-    return ( await getDecoddedToken())?.sub
+    return (await getDecoddedToken())?.sub
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,22 +4,27 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
-    const { pathname } = request.nextUrl;
-   
-   const authRoutes = ["/login", "/register"];
-   const protectedRoutes = ["/cart", "/checkout", "/profile"];
 
-   if (token && authRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-   }
-   if (!token && protectedRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
-    
-   }
+    const cookieName = process.env.NODE_ENV === "production" ?
+        "__Secure-next-auth.session-token" :
+        "next-auth.session-token";
+        
+    const token = await getToken({ req: request, cookieName });
+    const { pathname } = request.nextUrl;
+
+    const authRoutes = ["/login", "/register"];
+    const protectedRoutes = ["/cart", "/checkout", "/profile"];
+
+    if (token && authRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
+    if (!token && protectedRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL("/login", request.nextUrl));
+
+    }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-matcher: ["/cart", "/login", "/register"],
+    matcher: ["/cart", "/login", "/register"],
 }
